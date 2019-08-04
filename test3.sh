2511d2dd83355ace10940a3276ead75e53e042d9
goodvibesjumper <goodvibesjumper@hotmail.com>
2019-08-04 17:07:28 -0600

    .

--- test3.sh
+++ test3.sh
@@ -1,12 +1,12 @@
-curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"AVAIL\"}"}' localhost:8844/event
+curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"AVAIL\",\"lat\":\"-111.89\",\"long\":\"40.728\"}}"}' localhost:8844/event
 sleep 1
-curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"LOAD\"}"}' localhost:8844/event
+curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"AVAIL\",\"lat\":\"-111.79\",\"long\":\"40.728\"}"}' localhost:8844/event
 sleep 1
-curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"BREAK\"}"}' localhost:8844/event
+curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"LOAD\",\"lat\":\"-111.69\",\"long\":\"40.728\"}"}' localhost:8844/event
 sleep 1
-curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"OUTSVC\"}"}' localhost:8844/event
+curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"BREAK\",\"lat\":\"-111.59\",\"long\":\"40.728\"}"}' localhost:8844/event
 sleep 1
-curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"ENROUTE\"}"}' localhost:8844/event
+curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"OUTSVC\",\"lat\":\"-111.49\",\"long\":\"40.728\"}"}' localhost:8844/event
 sleep 1
-#curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"Location\":\"ENROUTE\"}"}' localhost:8844/event
-#sleep 1
+curl -H "Accept: application/json" -XPOST -d '{"eventName":"ev_update_vehicle","data":"{\"vehicle\":\"123\",\"status\":\"ENROUTE\",\"lat\":\"-111.39\",\"long\":\"40.728\"}"}' localhost:8844/event
+sleep 1
