359190a64fe80710ae3f6352204f13e8badc6bb1
goodvibesjumper <goodvibesjumper@hotmail.com>
2019-08-14 11:32:47 -0600

    WIP : now has a react-js client that will connect to the go websocket

--- ws-react-client-app/src/App.js
+++ ws-react-client-app/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client = new W3CWebSocket('ws://localhost:8844/ws');
+
+class App extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        Practical Intro To WebSockets.
+      </div>
+    );
+  }
 }
 
 export default App;
